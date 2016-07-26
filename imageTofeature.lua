38276a78fd29767d4c12f269aa53cdb6242cdb84
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-07-25 17:37:36 -0700

    a

--- imageTofeature.lua
+++ imageTofeature.lua
@@ -72,6 +72,6 @@ for _, img_path in ipairs(image_paths) do
 
   -- get features
   local features = model:get(53):forward(img:cuda()):squeeze() 
-  print(feature:type())
+  print(features:type())
   
 end
