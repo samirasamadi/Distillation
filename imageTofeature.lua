72c6d1f0bfdf5dabd7c3a6c0e30865a3349593a4
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-07-26 11:21:46 -0700

    a

--- imageTofeature.lua
+++ imageTofeature.lua
@@ -3,6 +3,8 @@ require 'cudnn'
 require 'cunn'
 local tablex = require 'pl.tablex'
 
+paths = logs/vgg
+
 if #arg < 2 then
   io.stderr:write('Usage: th example_classify.lua [MODEL] [FILE]...\n')
   os.exit(1)
