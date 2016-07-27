7bff5553e0ab1fe1246df79fb29f10cc2ca74e4f
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-07-27 14:38:49 -0700

    a

--- imageTofeature.lua
+++ imageTofeature.lua
@@ -35,7 +35,7 @@ provider = torch.load 'provider.t7'
 provider.trainData.data = provider.trainData.data:float()
 -- provider.trainData.data is a floatTensor
 local indices = torch.randperm(provider.trainData.data:size(1)):long():split(1)
-print(indices:size())
+print(indices:type())
 for t,v in ipairs(indices) do
   local inputs = provider.trainData.data:index(1,v)
   local targets = provider.trainData.labels:index(1,v)
