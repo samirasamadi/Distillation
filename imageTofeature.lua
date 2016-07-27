d5454255e800d00fcb88bf22f2ed0f5c4a581ce8
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-07-27 12:33:32 -0700

    aa

--- imageTofeature.lua
+++ imageTofeature.lua
@@ -34,9 +34,11 @@ print(c.blue '==>' ..' loading data')
 provider = torch.load 'provider.t7'
 provider.trainData.data = provider.trainData.data:float()
 
-local inputs = provider.trainData.data:index(1,v)
+length = provider.trainData.data:size(1)
+
+local inputs = provider.trainData.data:index(1, length)
 print('inputs:size()', inputs:size())
-local targets = provider.trainData.labels:index(1,v)
+local targets = provider.trainData.labels:index(1, length)
 print('targets:size()', targets:size())
 -- local outputs = model:forward(inputs)
 
