a64e2ef0df29f13aef0bfb4dddd6cc4c4bad7b63
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-08-02 14:42:01 -0700

    aa

--- example_classify.lua
+++ example_classify.lua
@@ -124,6 +124,7 @@ for _, img_path in ipairs(image_paths) do
   print('after layer 4')
   output2 = model:get(1):get(5):forward(output2)
   print('after layer 5')
+  print(output2)
   output2 = model:get(1):get(6):forward(output2)
    print('after layer 6')
   local finalOutput = model:get(2):forward(output2)
