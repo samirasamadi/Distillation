5736e691df4492cb52cddcb42c69f0f2ea3eda61
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-08-03 14:33:19 -0700

    aa

--- critical.lua
+++ critical.lua
@@ -33,7 +33,9 @@ for i = 1, length do
 	for j = i+1, length do
 		print(i, j)
 	    local feature_x = points[i][1]
+		print(feature_x)
 		local feature_y = points[j][1]
+		print(feature_y)
 		
 		local hardlabel_x = points[i][3]
 		local hardlabel_y = points[j][3]
