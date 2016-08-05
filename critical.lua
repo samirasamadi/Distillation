ee4b65904a9336f7b6e65128a716668d29154e65
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-08-05 15:16:14 -0700

    aa

--- critical.lua
+++ critical.lua
@@ -53,7 +53,7 @@ end
 ------------------------------------------------------------------------------------------------
 
 opt = lapp[[
---trainSize                (default 3)                           size of training set
+--trainSize                (default 5)                           size of training set
 ]]
 
 if #arg < 1 then
