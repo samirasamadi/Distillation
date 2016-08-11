31598a3150d300822e63f36291cfcc719000b7cb
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-08-10 17:03:46 -0700

    aa

--- imageTofeature.lua
+++ imageTofeature.lua
@@ -51,7 +51,7 @@ end
 print(model1)
 
 parameters,gradParameters = model1:getParameters()
-print('parameters of the model1 is', parameters:dim(2))
+print('parameters of the model1 is', parameters:size(2))
 
 
 -------------------------------------------------
