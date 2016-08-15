ac5626657bcf50412fbef4f74a1720e404f117c9
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-08-15 16:03:02 -0700

    aa

--- critical.lua
+++ critical.lua
@@ -64,7 +64,7 @@ end
 
 model_path = opt.model
 
-points = torch.load('points_table.dat')
+points = torch.load('train_featureTable.dat')
 -- points is a table. Each row of the table has three components: featureTensor, softLabels, hardLabel. Use points[i][1] to get feature vector of the ith training point 
 
 length = opt.trainSize
