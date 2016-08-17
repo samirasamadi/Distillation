f3dc04a92d5bc33e56c28d15238de5fa5bfa62b4
samirasamadi <samira.samadi@ssamadi-mbp.geneticfinance.com>
2016-08-17 13:21:01 -0700

    a

--- train_Distillation.lua
+++ train_Distillation.lua
@@ -123,7 +123,7 @@ function train()
       
       local outputs = model2:forward(inputs:view(1,512))
 	  print('output is', outputs)
-	  outputs:view(10,1)
+	  outputs:view(10)
 	  print('output is', outputs)
 	  -- outputs is a torch.CudaTensor of size 1x10
 	  
